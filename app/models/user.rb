57066565f4f0bcf1618b748a8a3b174d7ea690c6
takahiro902 <123475120+takahiro902@users.noreply.github.com>
2023-05-13 14:51:09 +0900

    コードレビュー後テストコード追加前まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[一-龥ぁ-んァ-ン]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-んァ-ン]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     japanese_user = Gimei.name
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Internet.password(min_length: 6, mix_case: true) }
     password_confirmation { password }
     last_name { japanese_user.last.kanji }
     first_name { japanese_user.first.kanji }
