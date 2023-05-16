bdf45ba8081a3bfa04d4f284ed09ddb6590134fb
takahiro902 <123475120+takahiro902@users.noreply.github.com>
2023-05-16 23:45:12 +0900

    モデルとテーブル作成

---
+++ db/migrate/20230516142518_items.rb
@@ -0,0 +1,15 @@
+class Items < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :name,        null: false
+      t.text       :description, null: false
+      t.integer    :category_id, null: false
+      t.integer    :status_id,   null: false
+      t.integer    :cost_id,     null: false
+      t.integer    :area_id,     null: false
+      t.integer    :shipment_id, null: false
+      t.integer    :proce,       null: false
+      t.references :user,        null: false, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_131449) do
+ActiveRecord::Schema.define(version: 2023_05_16_142518) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "cost_id", null: false
+    t.integer "area_id", null: false
+    t.integer "shipment_id", null: false
+    t.integer "proce", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2023_04_20_131449) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
