1a883f702eab3ac32f3ff11ba746af418a890e8f
takahiro902 <123475120+takahiro902@users.noreply.github.com>
2023-05-21 14:00:30 +0900

    正常系テスト：出品機能

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    name {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    status_id {Faker::Number.between(from: 2, to: 7)}
+    cost_id {Faker::Number.between(from: 2, to: 3)}
+    area_id {Faker::Number.between(from: 2, to: 48)}
+    shipment_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 300, to: 9_999_999)}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context "出品ができる場合" do
+      it "入力に問題がなければ出品ができる" do
+        expect(@item).to be_valid
+      end
+    end
+    context "出品ができない場合" do
+      it ""
+  end
 end
+
+
+#| name        | string     | null: false                    |
+#| description | text       | null: false                    |
+#| category_id | integer    | null: false                    |
+#| status_id   | integer    | null: false                    |
+#| cost_id     | integer    | null: false                    |
+#| area_id     | integer    | null: false                    |
+#| shipment_id | integer    | null: false                    |
+#| price       | integer    | null: false                    |
+#| user        | references | null: false, foreign_key: true |
