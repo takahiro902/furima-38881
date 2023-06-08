8914c36d4d660d70e811e5b70a4fa1f308dbaffa
takahiro902 <123475120+takahiro902@users.noreply.github.com>
2023-06-08 09:52:08 +0900

    Sold out!の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to root_path unless current_user.id == @item.user_id
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
